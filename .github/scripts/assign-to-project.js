d30a1a078ddd9a27baacdc8e578e7fc974af3a18
Ji-hoon Choi <akdlsz21@gmail.com>
2023-11-03 16:45:29 +0900

    added

---
+++ .github/scripts/assign-to-project.js
@@ -0,0 +1,26 @@
+async function assignToProject(github, context, columnId) {
+	const issue_number = context.issue.number;
+
+	try {
+		// Get the issue to retrieve its ID
+		const issue = await github.rest.issues.get({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			issue_number: issue_number,
+		});
+
+		// Create a card for the issue in the project column
+		await github.rest.projects.createCard({
+			column_id: columnId,
+			content_id: issue.data.id,
+			content_type: 'Issue',
+		});
+
+		console.log(`Issue ${issue_number} added to project column ${columnId}`);
+	} catch (error) {
+		console.error(`Error adding issue to project: ${error}`);
+		throw error; // This will fail the workflow step
+	}
+}
+
+module.exports = assignToProject;
